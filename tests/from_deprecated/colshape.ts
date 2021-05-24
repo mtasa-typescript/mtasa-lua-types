2fef07464f140ee845b605a111826549a240b626
Toliak <ibolosik@gmail.com>
2021-05-24 21:47:53 +0300

    tests wip

---
+++ tests/from_deprecated/colshape.ts
@@ -0,0 +1,7 @@
+import { mtasa } from '../../types/mtasa/client';
+
+const colCircle: mtasa.ColShape = mtasa.createColCircle(1, 1, 1);
+const colCuboid: mtasa.ColShape = mtasa.createColCuboid(13, 14, 15, 16, 17, 18);
+const colRectangle: mtasa.ColShape = mtasa.createColRectangle(0, 0, 1, 2);
+const colSphere: mtasa.ColShape = mtasa.createColSphere(13, 14, 15, 200);
+const colTube: mtasa.ColShape = mtasa.createColTube(4, 5, 6, 100, 100);
--- types/mtasa/client/oop/DxTexture.d.ts
+++ types/mtasa/client/oop/DxTexture.d.ts
@@ -95,4 +95,62 @@ export class DxTexture {
         mipmaps?: boolean,
         textureEdge?: string
     );
+
+    /**
+     * This function fetches the Texture_pixels|pixels from a texture element. It can be used
+     * with a standard texture, render target or screen source.
+     * *This function is slow and not something you want to be doing once a frame.
+     * *It is slower when reading pixels from a render target or screen source.
+     * *And is very slow indeed if the texture format is not  argb .
+     * @see {@link https://wiki.multitheftauto.com/wiki/DxGetTexturePixels Wiki, dxGetTexturePixels }
+     * @param texture The texture element to get the pixels from
+     * @param x Rectangle left position
+     * @param y Rectangle top position
+     * @param width Rectangle width
+     * @param height Rectangle height
+     * @return returns a plain format pixels string if successful, false if invalid arguments were
+     * passed to the function.
+     */
+    getPixels(
+        texture: Element,
+        x?: number,
+        y?: number,
+        width?: number,
+        height?: number
+    ): string;
+
+    /**
+     * This functions allows you to change the edge handling after creating the texture.
+     * @see {@link https://wiki.multitheftauto.com/wiki/DxSetTextureEdge Wiki, dxSetTextureEdge }
+     * @param textureEdge The texture edge mode. Available modes are wrap, mirror, clamp, border, mirror-once
+     * @param border-color If textureEdge is set to border, you are able to define a border color here
+     */
+    setEdge(
+        textureEdge: string,
+        border_color?: number
+    ): boolean;
+
+    /**
+     * This function sets the Texture_pixels|pixels of a texture element. It can be used with a
+     * standard texture, render target or screen source. Only plain format pixels please.
+     * * This function is slow and not something you want to be doing once a frame.
+     * * It is very slow when setting pixels to a render target or screen source.
+     * * And is very slow indeed if the texture format is not argb.
+     * @see {@link https://wiki.multitheftauto.com/wiki/DxSetTexturePixels Wiki, dxSetTexturePixels }
+     * @param texture The texture element to set the pixels of
+     * @param pixels The plain format pixels to use
+     * @param x Rectangle left position
+     * @param y Rectangle top position
+     * @param width Rectangle width
+     * @param height Rectangle height
+     * @return returns a string if successful, false if invalid arguments were passed to the function.
+     */
+    setPixels(
+        texture: Element,
+        pixels: string,
+        x?: number,
+        y?: number,
+        width?: number,
+        height?: number
+    ): boolean;
 }
