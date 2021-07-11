0b37e277d87628601f02aa8853ccfc0131c57d84
Toliak <toliakpurple@gmail.com>
2021-07-11 20:13:48 +0300

    Hotfix

--- types/mtasa/shared/utils/callback.d.ts
+++ types/mtasa/shared/utils/callback.d.ts
@@ -14,4 +14,4 @@
  * * 1007: File error <br>
  * @param arguments The arguments that were passed into fetchRemote
  */
-export type FetchRemoteCallback = (responseData: string, error: number, ...arguments: any[]) => void;
+export type FetchRemoteCallback = (this: void, responseData: string, error: number, ...arguments: any[]) => void;
